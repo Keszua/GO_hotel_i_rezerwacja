aaeca1fc41103bfcb93540e86e98e48a485371c5
kmichalczyk <k.michalczyk@radwag.pl>
2021-05-21 12:33:30 +0200

    Dodaj TemplateData do przekazywania danych na stronę

--- cmd/pkg/handlers/handlers.go
+++ cmd/pkg/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"example.com/udemy/cmd/pkg/config"
+	"example.com/udemy/cmd/pkg/models"
 	"example.com/udemy/cmd/pkg/render"
 )
 
@@ -29,10 +30,17 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.html")
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
 }
 
 // About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.html")
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	// send the data to the template
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
